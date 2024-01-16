9975f8f7084cbbe408b69c94b0d9ce0e6b8ad619
Sumaniya Devendra <dsumniya@gmail.com>
2024-01-16 20:04:26 +0530

    add new Bottom screen style and Navigation in Bottom tabs

---
+++ src/components/CustomSearch.js
@@ -0,0 +1,69 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import React from 'react';
+import {moderateScale, scale} from 'react-native-size-matters';
+import CustomIcon from './CustomIcon';
+import Color from '../constants/Color';
+
+const CustomSearch = ({
+  placeholder = '',
+  inputStyle = {},
+  onChangeText = () => {},
+  secureTextEntry,
+  onPressRight,
+  rightIcon,
+  autoFocus,
+}) => {
+  return (
+    <View style={styles.textInput}>
+      <TextInput
+        placeholder={placeholder}
+        style={{...styles.inputStyle, ...inputStyle}}
+        onChangeText={onChangeText}
+        secureTextEntry={secureTextEntry}
+        autoFocus={autoFocus}
+        autoCapitalize="none"
+        autoComplete="off"
+        autoCorrect={false}
+      />
+      {!!rightIcon && (
+        <View style={styles.rightIconContainer}>
+          <TouchableOpacity onPress={onPressRight}>
+            <CustomIcon name={rightIcon} size={20} color={'#000000'} />
+          </TouchableOpacity>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default CustomSearch;
+
+const styles = StyleSheet.create({
+  inputStyle: {
+    padding: moderateScale(12),
+    borderColor: Color.LIGHTGRAY,
+    fontSize: scale(14),
+    height: moderateScale(52),
+    fontFamily: 'Rubik-Regular',
+    fontWeight: '400',
+    fontStyle: 'normal',
+    lineHeight: 21,
+  },
+  textInput: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: moderateScale(12),
+    borderWidth: 0.5,
+    justifyContent: 'space-between',
+  },
+  rightIconContainer: {
+    marginRight: moderateScale(8),
+    alignItems: 'center',
+  },
+});
--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,14 +1,45 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import CustomSearch from '../../components/CustomSearch';
+import Color from '../../constants/Color';
+import styles from './Styles';
+import {moderateScale} from 'react-native-size-matters';
 
 const HomeScreen = () => {
+  const [searchValue, setSearchValue] = useState('');
+  let isPress = false;
+
+  const handleSearch = text => {
+    setSearchValue(text);
+  };
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <>
+      <View style={{flex: 1, backgroundColor: Color.WHITE}}>
+        <SafeAreaView style={styles.subContainer}>
+          <View style={{marginHorizontal: moderateScale(16)}}>
+            <View style={{flexDirection: 'row'}}>
+              <View>
+                <Text>Hello,</Text>
+                <Text>Juana Antonieta</Text>
+              </View>
+              <View>
+                <Text>Ghanti</Text>
+              </View>
+            </View>
+            <CustomSearch
+              inputStyle={{width: '90%'}}
+              placeholder="Search"
+              onChangeText={handleSearch}
+              value={searchValue}
+              rightIcon={!isPress ? 'search-outline' : ''}
+              onPressRight={() => {}}
+            />
+          </View>
+        </SafeAreaView>
+      </View>
+    </>
   );
 };
 
 export default HomeScreen;
-
-const styles = StyleSheet.create({});
